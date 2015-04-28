d71f6687167e05aced35ba6a1bfa9d9f1f2c5e7d
Rod McNew <rmcnew@relivinc.com>
2015-04-28 13:21:16 -0500

    use composer autoloader

--- src/Module.php
+++ src/Module.php
@@ -5,24 +5,12 @@
 
 namespace {ROOTNAMESPACE}\{NAMESPACE};
 
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 
 /**
  * Class Module
  */
-class Module implements AutoloaderProviderInterface
+class Module
 {
-    public function getAutoloaderConfig()
-    {
-        return [
-            'Zend\Loader\StandardAutoloader' => [
-                'namespaces' => [
-                    __NAMESPACE__ => __DIR__,
-                ],
-            ],
-        ];
-    }
-
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
